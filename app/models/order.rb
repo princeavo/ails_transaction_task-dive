0dc472f674b0bd0b04c941df68bc30f89677784b
AVOHOU Prince Boris <avohouprince@gmail.com>
2024-11-15 21:02:00 +0100

    first

---
+++ app/models/order.rb
@@ -0,0 +1,26 @@
+class Order < ApplicationRecord
+  belongs_to :user
+  has_many :ordered_lists
+  has_many :items, through: :ordered_lists
+  accepts_nested_attributes_for :ordered_lists
+
+  def update_total_quantity1
+    self.ordered_lists.each do |line_item|
+      item = Item.find_by(id: line_item.item_id)
+      item.total_quantity += line_item.quantity
+      item.save!
+    end
+  end
+  def update_total_quantity
+    # Encapsule la mise à jour dans une transaction
+    Item.transaction do
+      self.ordered_lists.each do |line_item|
+        item = Item.find_by(id: line_item.item_id)
+        item.with_lock do
+          item.total_quantity += line_item.quantity
+          item.save!
+        end
+      end
+    end
+  end
+end
